e1269dbe2dee116f3a383305d5c89f1de93ce3c4
Enrique M. Talavera <kiquetal@gmail.com>
2023-08-15 22:30:31 -0400

    adding input

---
+++ chapterv/inputs.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	fmt.Println(fmt.Sprintf("Hello, %s", name))
+}
--- chapterv/maps.go
+++ chapterv/maps.go
@@ -4,9 +4,24 @@ import "fmt"
 
 func main() {
 
-	mapa := map[string]int{
-		"one": 1,
-		"two": 2,
+	ages := map[string]int{}
+	ages["Kevin"] = 18
+	ages["John"] = 20
+	ages["Smith"] = 30
+	for k, v := range ages {
+		switch v {
+		case 18, 20:
+			fmt.Println(fmt.Sprintf("key: %s, value: %d", k, v))
+		default:
+			fmt.Println(fmt.Sprintf("The key %s is not allowed", k))
+
+		}
+
 	}
-	fmt.Println(mapa["one"])
+
+}
+
+func greeting(name, message string) (salution string) {
+	salution = fmt.Sprintf("%s, %s", name, message)
+	return
 }
